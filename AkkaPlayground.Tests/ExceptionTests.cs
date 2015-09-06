05380bbee8772efe9b09dd1479c3300db6fad3b6
Stephen Kennedy <stephen@devenable.com>
2015-09-06 17:17:05 +1000

    Added ExceptionalActor to intercept exceptions and read them as messages

--- AkkaPlayground.Tests/ExceptionTests.cs
+++ AkkaPlayground.Tests/ExceptionTests.cs
@@ -13,14 +13,54 @@ namespace AkkaPlayground.Tests
         }
     }
 
+    internal class UnhandledExceptionMessage
+    {
+        public Exception Exception { get; private set; }
+
+        public UnhandledExceptionMessage(Exception ex)
+        {
+            Exception = ex;
+        }
+    }
+
+    public class ExceptionalActor : UntypedActor
+    {
+        private readonly IActorRef _probe;
+
+        public IActorRef SupervisedActor { get; private set; }
+
+        public ExceptionalActor(IActorRef probe, Props supervisedActorProps)
+        {
+            _probe = probe;
+            SupervisedActor = Context.ActorOf(supervisedActorProps, "child");
+        }
+
+        protected override SupervisorStrategy SupervisorStrategy()
+        {
+            return new OneForOneStrategy(-1, TimeSpan.FromSeconds(3), x =>
+            {
+                _probe.Tell(new UnhandledExceptionMessage(x));
+
+                return Directive.Resume;
+            });
+        }
+
+        protected override void OnReceive(object message)
+        {
+            SupervisedActor.Forward(message);
+        }
+    }
+
     public class ExceptionTests : TestKit
     {
         [Fact]
         public void Do_Khaboom()
         {
-            IActorRef bad = ActorOf<MyBadActor>();
-            bad.Tell("boom");
-            ExpectMsg<Failure>();
+            var probe = CreateTestProbe("ExceptionProbe");
+            var exceptionalActor = ActorOfAsTestActorRef<ExceptionalActor>(Props.Create(() => new ExceptionalActor(probe, Props.Create<MyBadActor>())));
+            exceptionalActor.Tell("It's a thing!");
+
+            probe.ExpectMsg<UnhandledExceptionMessage>();
         }
     }
 }
